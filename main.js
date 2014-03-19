ac7297944a90b7479aaeac7541ea01c9e77b3287
dgmp88 <dgmprichard@gmail.com>
2014-03-19 22:13:29 +0000

    Basic login
    Basic login system added. Commenting is only possible if logged in.
    Logging comments not working. Annotating possible regardless of logged
    in/out

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 Comments = new Meteor.Collection("comments");
 
-if (Meteor.isClient) {
+Comments.allow( {
+  insert : function () {
+    return !(Meteor.user()==null);
+  }
+})
 
+
+
+if (Meteor.isClient) {
   Template.comments.comment = function () {
     console.log(Comments.find({},  {sort: { date : -1 } }))
     return Comments.find({},  {sort: { date : -1 } });
@@ -14,7 +21,8 @@ Template.addbutton.events = {
     console.log(comment_text)
     Comments.insert({
       comment_text: comment_text.val(),
-      date: new Date().getTime()
+      date: new Date().getTime(),
+    //  "userEmail": Meteor.user().emails[0].address
     });
     comment_text.val("")
   }
